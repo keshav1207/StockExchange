91c089eb50a13f613571defd77a46c785914e5e2
Keshav Callychurn <keshav0799@gmail.com>
2026-01-11 20:01:06 -0500

    Add modules and their pom.xml

---
+++ price-consumer-service/src/main/java/PriceConsumerServiceApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceConsumerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PriceConsumerServiceApplication.class, args);
+    }
+}
