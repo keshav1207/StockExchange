d382b42fef0118e9013ae8eb542cddd167e51952
Keshav Callychurn <keshav0799@gmail.com>
2026-01-17 16:06:00 -0500

    Create kafkaProducerConfig

---
+++ stock-price-service/src/main/java/config/KafkaProducerConfig.java
@@ -0,0 +1,18 @@
+package config;
+
+import com.common.dto.StockPrice;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public KafkaTemplate<String, StockPrice> kafkaTemplate(
+            ProducerFactory<String,StockPrice> producerFactory){
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
